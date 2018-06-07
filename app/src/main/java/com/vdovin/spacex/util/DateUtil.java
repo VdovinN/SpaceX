eaf5e2ea059afd66f24716e0f7f10ef5f3f0c359
WhiteDemon <vdovinnick@gmail.com>
2018-06-07 16:35:19 +0300

    Fix problem with simpledateformat on some devices;

--- app/src/main/java/com/vdovin/spacex/util/DateUtil.java
+++ app/src/main/java/com/vdovin/spacex/util/DateUtil.java
@@ -2,12 +2,13 @@ package com.vdovin.spacex.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateUtil {
 
     public static String convertTimestampToFormattedDate(Long timestamp) {
         Date date = new Date(timestamp);
-        SimpleDateFormat formatter = new SimpleDateFormat("EEEE dd, YYYY");
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE dd, yyyy", Locale.getDefault());
         return formatter.format(date);
     }
 
