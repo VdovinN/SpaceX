c6bed996e676cd2eb72196b3e4d77eb768f421d5
WhiteDemon <xxunknownxx@mail.ru>
2020-07-27 13:29:54 +0300

    Implement dagger hilt dependencies;

--- app/src/main/java/com/vdovin/spacex/MainActivity.java
+++ app/src/main/java/com/vdovin/spacex/MainActivity.java
@@ -1,6 +1,6 @@
 package com.vdovin.spacex;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
